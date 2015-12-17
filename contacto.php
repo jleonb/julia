b4addde53db7cb1deaf7c3ede994a85f29abdee0
Jaime <jleon@webframe.cl>
2015-12-17 17:18:00 -0300

    Final
    Signed-off-by: Jaime <jleon@webframe.cl>

--- contacto.php
+++ contacto.php
@@ -2,8 +2,8 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "jleon@webframe.cl";
-    $email_subject = "[CONTACTO - WEB]";
+    $email_to = "pgarrido@julia.cl";
+    $email_subject = "CONTACTO - JULIA.CL";
  
     function died($error) {
         // your error code can go here
@@ -20,17 +20,17 @@ if(isset($_POST['email'])) {
     $comments = $_POST['comments']; // required
  
 
-    $email_message = "Los datos ingresados son:\n\n";
+    $email_message = "Los datos del interesado son:\n\n";
  
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
     }
  
-    $email_message .= "Nombre: ".clean_string($nombre)."\n\n";
-    $email_message .= "Telefono: ".clean_string($telefono)."\n\n";
+    $email_message .= "Nombre: ".clean_string($nombre)."\n";
+    $email_message .= "Teléfono: ".clean_string($telefono)."\n";
     $email_message .= "Correo: ".clean_string($email)."\n\n";  
-    $email_message .= "Comentario: ".clean_string($comments)."\n\n";
+    $email_message .= "Comentario: ".clean_string($comments)."\n";
  
 // create email headers
 $headers = 'From: '.$email."\r\n".
@@ -38,9 +38,20 @@ $headers = 'From: '.$email."\r\n".
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
 sleep(2);
-echo "<meta http-equiv='refresh' content=\"0; url=gracias.html\">";
+echo "<meta http-equiv='refresh' content=\"0; url=./gracias.html\">";
 ?>
  
 <?php
 }
 ?>
+
+
+
+
+
+
+
+
+
+
+
